30bdcce2f79b1646bee237c41a531d45b7eab8f8
Hugo <Hugo_a_roman@hotmail.com>
2022-08-16 17:02:51 -0600

    fix register

--- Client.py
+++ Client.py
@@ -1,8 +1,9 @@
 import logging
-from getpass import getpass
 from argparse import ArgumentParser
 import slixmpp 
 from slixmpp.exceptions import IqError, IqTimeout
+from aioconsole import ainput
+
 
 logging.basicConfig(level=logging.DEBUG, format="%(levelname)-8s %(message)s")
 
@@ -16,7 +17,7 @@ class Client(slixmpp.ClientXMPP):
         self.password = password
         self.add_event_handler("session_start", self.Start)
         self.add_event_handler("register", self.register)
-        self.add_event_handler("message", self.Message)
+        self.add_event_handler("message", self.Messageto)
         
     async def Start(self, event):
         
@@ -49,44 +50,54 @@ class Client(slixmpp.ClientXMPP):
         def AddUsers():
             new_user = input("Ingrese nuevo usuario: ")
             self.send_presence_subscription(pto=new_user)
+            message="Hi new friend!"
+            self.send_message(mto=new_user, mbody=message, mtype="chat", mfrom=self.boundjid.bare)
             print("---- Agregado exitosamente ----")
             
             
         def user_info():
             self.get_roster()
-            usuario = input("Ingrese el usuario: ")
-            name = self.client_roster[usuario]['name']
-            print('\n %s (%s)' % (name, usuario))
 
-            usuarios = self.client_roster.presence(usuario)
-            for res, pres in usuarios.items():
-                show = 'chat'
+            contact_user = input("Contact username: ")
+
+            name = self.client_roster[contact_user]['name']
+            print('\n %s (%s)' % (name, contact_user))
+
+            connections = self.client_roster.presence(contact_user)
+
+            if connections == {}:
+                print("           xa")
+                print("    No recent session")
+    
+            for res, pres in connections.items():
+                show = 'available'
                 if pres['show']:
                     show = pres['show']
-                print("     INFO:")
                 print('   - ', res, ' - ', show)
-                print('        ', show)
-                print('     Estado: ', pres['status'])
+                print('       ',  pres['status'])
             
         
         def Privatemessage():
             self.register_plugin("xep_0085")
-            recipient = input("Usuario destinatario: ")
-            msg = input("Escriba el mensaje: ")
-            self.send_message(mto=recipient, mbody=msg, mtype='chat')
-            #notification(recipient, "paused")
-            print("Se ha enviado el mensaje correctamente") 
-            
-        def Groupmessage():
-            self.register_plugin('xep_0030')
-            self.register_plugin('xep_0045') #Implements XEP-0045 Multi-User Chat
-            self.register_plugin('xep_0199')
 
-            room = input("Nombre de la sala: ")
+            recipient = input("Recipient: ")
+            notification(recipient, "composing")
+            message = input("Message: ")
+
+            self.send_message(mto=recipient, mbody=message, mtype="chat")
+            notification(recipient, "paused")
+            print("Message sent!")
+            
+        async def Groupmessage():
+            #self.register_plugin('xep_0030')
+            self.register_plugin('xep_0045') #Implements XEP-0045 Multi-User Chat
+            #self.register_plugin('xep_0199')
+
+            room = str(await ainput("Nombre de la sala: "))
             nickname = input("Nickname: ")
             message = input('Mensaje: ')
-            self.plugin['xep_0045'].join_muc(room+"@conference.alumchat.fun", nickname)
-            self.send_message(mto=room+"@room.alumchat.fun", mbody=message, mtype='groupchat')
+            self.plugin['xep_0045'].join_muc(room, nickname)
+            self.send_message(mto=room, mbody=message, mtype='groupchat')
             
         def state():
             estado = input("Indica el estado que desees (chat, desconectado): ")
@@ -103,7 +114,7 @@ class Client(slixmpp.ClientXMPP):
             
         def closeSesion():
             self.disconnect()
-            loginloop = False
+           
             print("Ha cerrado sesion exitosamente")
             
         def delete():
@@ -112,7 +123,6 @@ class Client(slixmpp.ClientXMPP):
             self.register_plugin('xep_0077')
             self.register_plugin('xep_0199')
             self.register_plugin('xep_0066')
-
             eliminar = self.Iq()
             eliminar['type'] = 'set'
             eliminar['from'] = self.boundjid.user
@@ -132,7 +142,7 @@ class Client(slixmpp.ClientXMPP):
             print("7. Files")
             print("8. Log out") 
             print("9. Delete account") 
-            print("------")
+           
             
             options = int(input("Elige una opcion: "))
             
@@ -153,6 +163,7 @@ class Client(slixmpp.ClientXMPP):
                 #await self.upload_fileee()
             elif options == 8:
                 closeSesion()
+                menu = False
             elif options == 9:
                 delete()
                 
@@ -162,4 +173,93 @@ class Client(slixmpp.ClientXMPP):
             await self.get_roster()
             
             
+    async def register(self, iq):
+        #self.send_presence()
+        #self.get_roster()
+        
+        resp = self.Iq()
+        resp['type'] = 'set'
+        resp['register']['username'] = self.boundjid.user
+        resp['register']['password'] = self.password
+
+        try:
+            await resp.send()
+            logging.info("Account created for %s!" % self.boundjid)
+        except IqError as e:
+            logging.error("Could not register account: %s" %
+                    e.iq['error']['text'])
+            self.disconnect()
+            
+        except IqTimeout:
+            logging.error("No response from server.")
+            self.disconnect()
+            
+    def Messageto(self, message):
+        print(str(message["from"]), ":  ", message["body"])
+
+    async def send_file(self):
+        recipient = input("A quien va dirigido: ")
+        filename = input("Archivo: ")
+
+        logging.info('Uploading file %s...', filename)
+        try:
+            url = await self['xep_0363'].upload_file(filename, domain=None, timeout=10)
+        except IqTimeout:
+            raise TimeoutError('Could not send message in time')
+        except IqError as e:
+            print(e)
+
+        logging.info('Upload success!')
+        logging.info('Sending file to %s', recipient)
+
+        message = self.make_message(mto=recipient, mbody=url)
+        message.send()
+        
     
+def register(username, password):
+    
+    
+    xmpp = Client(username, password)
+
+    xmpp.register_plugin("xep_0030")
+    xmpp.register_plugin("xep_0004")
+    xmpp.register_plugin("xep_0199")
+    xmpp.register_plugin("xep_0066")
+    xmpp.register_plugin("xep_0077")
+    xmpp["xep_0077"].force_registration = True
+
+    xmpp.connect()
+    xmpp.process()
+
+def login(username, password):
+    xmpp = Client(username, password)
+    xmpp.register_plugin("xep_0030")
+    xmpp.register_plugin("xep_0199")
+    xmpp.register_plugin('xep_0363')
+    xmpp.connect()
+    xmpp.process(forever=False)
+
+
+Menu2 = True
+while Menu2:
+    print("""
+            \r1 Sign Up
+            \r2 Log In
+            \r3 Exit    
+        """)
+    opcion = int(input("Eliga una opcion: "))
+    
+    if opcion == 1:
+        user = input("Username: ")
+        password = input("Password: ")
+        register(user, password)
+
+    elif opcion == 2:
+        user = input("Username: ")
+        password = input("Password: ")
+        login(user, password)
+        
+    elif opcion == 3:
+        loop = False
+    else:
+        print("Opcion Invalida")
